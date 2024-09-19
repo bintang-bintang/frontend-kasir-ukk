3fa332b9e3a453cf8f23bc9e170f1baadecc8eb5
bintang-bintang <bintangajiwibowo@gmail.com>
2024-09-19 09:42:23 +0700

    Commit pertama

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Login from './pages/Login'
+import Nopage from './pages/Nopage'
+import ProtectedRoute from './pages/ProtectedRoute'
+
+import AdminHome from './pages/admin/AdminHome'
+import CustomerHome from './pages/admin/AdminHome'
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Nopage />} />
+
+        {/* Rute jika role admin */}
+        <Route path='/admin' element={
+          <ProtectedRoute>
+            <AdminHome />
+          </ProtectedRoute>
+        } />
+
+        {/* Rute jika role customer */}
+        <Route path='/customer' element={
+          <ProtectedRoute>
+            <CustomerHome />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div>Penambahan home</div>
-  )
-}
-
-export default Home
